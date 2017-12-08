5d6ac1b9e4f3f425bde9df5b25afc9b5c63f41f8
lily <lily0132@users.noreply.github.com>
2017-12-08 14:14:43 +0800

    更新

--- christmas/js/christmas.js
+++ christmas/js/christmas.js
@@ -1,1 +1,55 @@
-require(["common"],function(){require(["jquery"],function(e){e(function(){var t=document.getElementById("music-audio");e("#sound").bind("click",function(){e(this).hasClass("pause")?t.play():t.pause(),e(this).toggleClass("pause")})})})}),define("christmas",function(){});
+requirejs.config({
+    //baseUrl:'/html/cbc-client/android/js/share',
+    baseUrl:'/js/share',
+    paths:{
+        'jquery': 'jquery'
+    },
+    shim:{
+        'jquery':{
+            exports: '$'
+        }
+    }
+});
+
+require([
+    'jquery',
+    'swiper'
+],
+function($, Swipe) {
+    $(function(){
+
+        if($('.slider ul li').length > 1){
+            var slider_len = $('.slider ul li').length;
+            for(var i=0;i<slider_len;i++){
+                $('#slider .dot').append(i==0?'<span class="current"></span>':'<span></span>');
+            }
+
+            var islider = new Swipe(document.querySelector('.slider'),{
+                auto: 0,
+                transitionEnd: function(index, elem) {
+                    if(slider_len == 2){
+                        if(index == 2){
+                            index = 0;
+                        }else if(index==3){
+                            index = 1;
+                        }
+                        lazyload.addImages(document.querySelectorAll('img.video-img'));
+                    }
+                    $('#slider .dot span').removeClass('current').eq(index).addClass('current');
+                }
+            });
+        }
+
+        var x = document.getElementById("music-audio");
+
+        $('#sound').bind('click', function(){
+            if($(this).hasClass('pause')){
+                x.play();
+            }else{
+                x.pause();
+            }
+            $(this).toggleClass('pause');
+        });
+
+    });
+});
